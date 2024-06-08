a60226525d8976bb7bf1fa704c89f51dd50389b6
The0Mikkel <mikkel.alb@gmail.com>
2024-06-09 00:02:33 +0200

    upgrade usage of Docker Compose to run using `docker compose` instead of `docker-compose`

--- scripts/nginx-reverse-proxy.sh
+++ scripts/nginx-reverse-proxy.sh
@@ -3,20 +3,19 @@ set -o allexport
 source .env
 set +o allexport
 
-while getopts d: flag
-do
+while getopts d: flag; do
     case "${flag}" in
-        d) web_dir=${OPTARG};;
-        *)
+    d) web_dir=${OPTARG} ;;
+    *) ;;
     esac
 done
 
 # Check docker is running and can be accessed
-if docker > /dev/null 2>&1 ; then
+if docker >/dev/null 2>&1; then
     :
 else
-    echo -e "${YELLOW}Docker is not running.${NC}";
-    exit 1;
+    echo -e "${YELLOW}Docker is not running.${NC}"
+    exit 1
 fi
 
 # Script to setup nginx reverse proxy in the web_dir directory
@@ -24,14 +23,14 @@ if [[ ! "$(docker ps -q -f name=nginx-proxy)" && ! "$(docker ps -q -f name=nginx
     echo ""
     echo -e "${BLUE}setting up nginx reverse proxy $NC"
 
-    if [ -d "${web_dir}"/nginx-reverse-proxy ]; then 
+    if [ -d "${web_dir}"/nginx-reverse-proxy ]; then
         echo "A previus nginx proxy installation is already located in ${web_dir} and not running. To setup a new nginx reverse proxy, the old installetion needs to be removed."
         echo "Do you want to proceed? [y]"
         read decision
         if [ "$decision" != "y" ]; then
 
-            echo -e "${YELLOW}Stopping program!${NC}";
-            exit 1;
+            echo -e "${YELLOW}Stopping program!${NC}"
+            exit 1
 
         fi
         echo "Removing nginx reverse proxy data and folder... (This action is using sudo. To prevent use of sudo, quit action and delete direcoroty '${web_dir}/nginx-reverse-proxy' before running this program again)"
@@ -47,7 +46,7 @@ if [[ ! "$(docker ps -q -f name=nginx-proxy)" && ! "$(docker ps -q -f name=nginx
     mkdir "${web_dir}"/nginx-reverse-proxy/vhost.d
     mkdir "${web_dir}"/nginx-reverse-proxy/fallback
     mkdir "${web_dir}"/nginx-reverse-proxy/acme
-    
+
     echo "Dowloading and creating files..."
     # Docker compose file
     cp "${gitdir}"/docker/nginx-proxy/docker-compose.yml "${web_dir}"/nginx-reverse-proxy/docker-compose.yml
@@ -56,10 +55,10 @@ if [[ ! "$(docker ps -q -f name=nginx-proxy)" && ! "$(docker ps -q -f name=nginx
     cp "$gitdir"/configuration/nginx-proxy/hardening.conf "${web_dir}"/nginx-reverse-proxy/conf.d/hardening.conf
 
     # Update nginx.tmpl: Nginx configuration file template
-    curl -s https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl> "${web_dir}"/nginx-reverse-proxy/nginx.tmpl
+    curl -s https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl >"${web_dir}"/nginx-reverse-proxy/nginx.tmpl
 
     # Inserting fallback page
-    echo "Something seems off. Did you land the right place?" > "${web_dir}"/nginx-reverse-proxy/fallback/index.html
+    echo "Something seems off. Did you land the right place?" >"${web_dir}"/nginx-reverse-proxy/fallback/index.html
 
     echo ""
     echo -e "${BLUE}starting nginx reverse proxy${NC}"
@@ -71,15 +70,15 @@ if [[ ! "$(docker ps -q -f name=nginx-proxy)" && ! "$(docker ps -q -f name=nginx
         echo "nginx-proxy network exists."
     fi
 
-    # Creates the reverse proxy with the 
+    # Creates the reverse proxy with the
     # nginx, nginx-gen and nginx-letsencrypt containers
-    cd ${web_dir}/nginx-reverse-proxy/ || ( echo "Error entering nginx-reverse-proxy/nginx-proxy folder" && exit )
-    docker-compose up -d
-    cd /$gitdir || ( echo "Error entering $gitdir folder" && exit )
+    cd ${web_dir}/nginx-reverse-proxy/ || (echo "Error entering nginx-reverse-proxy/nginx-proxy folder" && exit)
+    docker compose up -d
+    cd /$gitdir || (echo "Error entering $gitdir folder" && exit)
 
     echo ""
     echo -e "${GREEN}Nginx proxy have been deployed!${NC}"
-else 
+else
     echo ""
     echo -e "${YELLOW}Nginx proxy is already deployed!${NC}"
 fi
